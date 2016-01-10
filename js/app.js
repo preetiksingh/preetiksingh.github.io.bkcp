ceb93e2d1545abb3d9c6fb3659467ef2f5c1ab3e
preetiksingh <preetisingh41451@gmail.com>
2016-01-10 20:58:47 +0530

    Added Skills section

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+    $(document).ready(function(){
 
-$(function(){
     $('#project-ul-custom').mixItUp();
-});
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+    });
+
+    var ctx = $("#mycanvas").get(0).getContext("2d");
+    //pie chart data
+    //sum of values = 360
+    var data = [
+    {
+    value: 270,
+    color: "cornflowerblue",
+    highlight: "lightskyblue",
+    label: "Html/Css"
+    },
+    {
+    value: 50,
+    color: "lightgreen",
+    highlight: "yellowgreen",
+    label: "Javascript"
+    },
+    {
+    value: 40,
+    color: "orange",
+    highlight: "darkorange",
+    label: "Ruby"
+    }
+    ];
+    //draw
+    var piechart = new Chart(ctx).Pie(data);
+    });
